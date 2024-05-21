0143a7ff39e21f5af2e7704e6a76e5370f80fc04
krishna uttej kondisetty <uttej38505@gmail.com>
2024-05-21 17:14:19 -0500

    Add files via upload

---
+++ src/main/java/in/krishna/SpringbootExcelExportApplication.java
@@ -0,0 +1,13 @@
+package in.krishna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExcelExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExcelExportApplication.class, args);
+	}
+
+}
