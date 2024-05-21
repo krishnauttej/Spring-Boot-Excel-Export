1236c6370de57ed87be4a86b372abc76b66c7624
krishna uttej kondisetty <uttej38505@gmail.com>
2024-05-21 17:13:17 -0500

    Delete apache_poi_java_app-main directory

--- apache_poi_java_app-main/src/main/java/in/ashokit/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package in.ashokit;
-
-import java.io.Serializable;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Serializable> {
-
-}
